9c05480b53c086561083dcbecb5c934842ccf4a0
Jay-Jay-O <orpillajesusjames@gmail.com>
2024-12-10 22:29:28 +0800

    feat: idk

--- src/main/kotlin/com/example/regform/HelloApplication.kt
+++ src/main/kotlin/com/example/regform/HelloApplication.kt
@@ -8,7 +8,7 @@ import javafx.stage.Stage
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 1200.0, 800.0)
+        val scene = Scene(fxmlLoader.load(), 1200.0, 750.0)
         stage.title = "Form!"
         stage.scene = scene
         stage.show()
